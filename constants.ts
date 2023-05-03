9eeeb438c02c854c54def148a79d5d5c3ea0214b
dev <oldaidevs@proton.me>
2023-05-03 03:34:46 +0200

    add ca

--- constants.ts
+++ constants.ts
@@ -1,2 +1,2 @@
 export const JSON_RPC_URL = 'https://mainnet.infura.io/v3/1f9d33a510e443dc8ffe3721d5affe2d'
-export const baconTokenCA = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+export const baconTokenCA = '0x5dbac6aefe501ff1c0a253f9f29d353470d80b84'
