bf16ad5abeca7213f36a27c160c650d9a65bb719
dev <oldaidevs@proton.me>
2023-05-03 03:04:28 +0200

    last

--- constants.ts
+++ constants.ts
@@ -1,2 +1,2 @@
 export const JSON_RPC_URL = 'https://mainnet.infura.io/v3/1f9d33a510e443dc8ffe3721d5affe2d'
-export const baconTokenCA = '0x993a1b91E8D7606cA8210fACcc64C053140F73F2'
+export const baconTokenCA = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
