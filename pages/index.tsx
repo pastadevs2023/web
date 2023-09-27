3209d91235696769798d9c28b16f26640ad1eb92
pastadevs2023 <toxico@skiff.com>
2023-09-28 01:15:06 +0200

    final push

--- pages/index.tsx
+++ pages/index.tsx
@@ -85,9 +85,42 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>$CopyPasta</title>
-        <meta name="description" content="Uniswap Widgets" />
-        <link rel="icon" href="/bbaico.png" />
+        <title>
+          $CopyPasta | What the fuck did you just fucking say about me, you little bitch? Ill have you know I graduated
+          top of my class in the Navy Seals, and Ive been involved in numerous secret raids on Al-Quaeda, and I have
+          over 300 confirmed kills. I am trained in gorilla warfare and Im the top sniper in the entire US armed forces.
+          You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which
+          has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that
+          shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies
+          across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm
+          that wipes out the pathetic little thing you call your life. Youre fucking dead, kid. I can be anywhere,
+          anytime, and I can kill you in over seven hundred ways, and thats just with my bare hands. Not only am I
+          extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine
+          Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you
+          little shit. If only you could have known what unholy retribution your little clever comment was about to
+          bring down upon you, maybe you would have held your fucking tongue. But you couldnt, you didnt, and now youre
+          paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. Youre fucking
+          dead, kiddo.
+        </title>
+        <meta
+          name="description"
+          content="What the fuck did you just fucking say about me, you little bitch? Ill have you know I graduated
+          top of my class in the Navy Seals, and Ive been involved in numerous secret raids on Al-Quaeda, and I have
+          over 300 confirmed kills. I am trained in gorilla warfare and Im the top sniper in the entire US armed
+          forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes
+          of which has never been seen before on this Earth, mark my fucking words. You think you can get away with
+          saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network
+          of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The
+          storm that wipes out the pathetic little thing you call your life. Youre fucking dead, kid. I can be
+          anywhere, anytime, and I can kill you in over seven hundred ways, and thats just with my bare hands. Not only
+          am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States
+          Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent,
+          you little shit. If only you could have known what unholy retribution your little clever comment
+          was about to bring down upon you, maybe you would have held your fucking tongue. But you couldnt, you didnt,
+          and now youre paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it.
+          Youre fucking dead, kiddo."
+        />
+        <link rel="icon" href="/web.png" />
       </Head>
       <BaconFloating count={10} />
       <main className={styles.content_container}>
@@ -259,7 +292,7 @@ const Home: NextPage = () => {
                 <path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path>
               </svg>
             </a>
-            <a href="https://t.me/CopyPastaportal">
+            <a href="https://t.me/copypasta_portal">
               <svg
                 stroke="currentColor"
                 fill="white"
